c98582dd756cbcdb46ce9e1734f30221988e5a55
Ogwenya <emanuelogwenya@outlook.com>
2023-03-12 00:03:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const writeCards = (arr, msg) => {
+	let result = [];
+	for (let i = 0; i < arr.length; i++) {
+		let message = `Thank you, ${arr[i]}, for the wonderful ${msg} gift!`;
+		result.push(message);
+	}
+	return result;
+};
+
+const countDown = (num) => {
+	while (num >= 0) {
+		console.log(num);
+		num--;
+	}
+};
